db0e792f1268fb561bcab1375308e5cf6df0d339
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-08-09 16:32:34 +0200

    fix: do not set timezone

--- cmd/promi/main.go
+++ cmd/promi/main.go
@@ -21,7 +21,6 @@ func main() {
 	b, err := king.NewBuildInfo(version,
 		king.WithDateString(date),
 		king.WithRevision(commit),
-		king.WithLocation("Europe/Zurich"),
 	)
 	if err != nil {
 		l.Fatal(err)
